117b0a79bd59e1ac8c6d3970f63ff365ad1de610
akshayhirpara <akshayhirpara248@gmail.com>
2025-03-08 08:31:58 +0530

    Add files via upload

---
+++ H/doloop.java
@@ -0,0 +1,13 @@
+class doloop
+{
+	public static void main (String[] args)
+	{
+		int a=5;
+		do
+		{
+		System.out.println("do");
+		}
+		while(a<3);
+	}
+}	
+	
---
+++ H/sums.java
@@ -0,0 +1,9 @@
+class sums
+{
+	public static void main(String[] args)
+	{
+		int num1=5, num2=10, sumb;
+		sumb = num1 + num2;
+		System.out.println("sum of "+sumb);
+	}
+}	
